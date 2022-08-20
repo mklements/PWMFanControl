63defe12bb3fc228e4671c7287fff42be7bead34
Michael Klements <82847422+mklements@users.noreply.github.com>
2022-08-21 07:52:46 +1000

    Add files via upload

---
+++ FanStepped.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as IO          #Calling GPIO to allow use of the GPIO pins
+import time                    #Calling time to allow delays to be used
+import subprocess              #Calling subprocess to get the CPU temperature
+
+IO.setwarnings(False)          #Do not show any GPIO warnings
+IO.setmode (IO.BCM)            #BCM pin numbers - PIN8 as ‘GPIO14’
+IO.setup(14,IO.OUT)            #Initialize GPIO14 as our fan output pin
+fan = IO.PWM(14,100)            #Set GPIO14 as a PWM output, with 50Hz frequency (this should match your fans specified PWM frequency)
+fan.start(0)                   #Generate a PWM signal with a 0% duty cycle (fan off)
+
+def get_temp():                #Function to read in the CPU temperature and return it as a float in degrees celcius
+    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+    temp_str = output.stdout.decode()
+    try:
+        return float(temp_str.split('=')[1].split('\'')[0])
+    except (IndexError, ValueError):
+        raise RuntimeError('Could not get temperature')
+
+while 1:                                     #Execute loop forever
+    temp = get_temp()                        #Get the current CPU temperature
+    if temp > 70:                            #Check temperature threshhold, in degrees celcius
+        fan.ChangeDutyCycle(100)             #Set fan duty based on temperature, from 0% (off) to 100% (full speed)
+    elif temp > 60:
+        fan.ChangeDutyCycle(85)
+    elif temp > 50:
+        fan.ChangeDutyCycle(70)
+    elif temp > 40:
+        fan.ChangeDutyCycle(50)
+    elif temp > 30:
+        fan.ChangeDutyCycle(25)
+    elif temp > 20:
+        fan.ChangeDutyCycle(10)
+    else:
+        fan.ChangeDutyCycle(0)
+    time.sleep(5)                           #sleep for 5 seconds
